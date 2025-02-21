0ea2f5589ef634dfc5804f3c4203abf77991c677
Rakim <rakimdevcraig@gmail.com>
2025-02-21 01:38:21 -0500

    new build

--- index.js
+++ index.js
@@ -7,14 +7,32 @@ const cors = require("cors");
 const Person = require("./models/person");
 // let data = require("./data");
 
-morgan.token("body", (getId = (req) => JSON.stringify(req.body)));
+morgan.token("body", (getBody = (req) => JSON.stringify(req.body)));
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
-app.use(express.static("dist"));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+function getDocs() {}
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -26,54 +44,86 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then((individual) => {
-    res.json(individual);
-  });
+  Person.findById(id)
+    .then((individual) => {
+      if (individual) {
+        res.json(individual);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 
-// function nameExists(name, array) {
-//   let nameCheck = array.filter((item) => item.name === name);
-//   let result = nameCheck[0] ? true : false;
-//   return result;
-// }
+app.get("/info", (req, res) => {
+  async function getCollectionLength() {
+    try {
+      const count = await Person.countDocuments({});
+      res.send(
+        `<p>PhoneBook has info for ${count} people</p> <p>${Date()}</p>`
+      );
+      return count;
+    } catch (error) {
+      console.error("Error counting documents:", error);
+      throw error;
+    }
+  }
 
-app.post("/api/persons", (req, res) => {
+  getCollectionLength();
+});
+
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
-    res.statusMessage = "This type of request needs a name AND Number";
+    // res.statusMessage = "This type of request needs a name AND Number";
     //we have to return or the code will move on to the next line and
     //still add the entry even though we generated a status code
-    return res.status(400).end();
+    return res.status(400).json({
+      error: "This type of request needs a name AND Number",
+    });
   }
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((err) => next(err));
+});
 
-  // if (nameExists(body.name, data.entries)) {
-  //   res.statusMessage =
-  //     "This name exists please make a request with a new name";
-  //   return res.status(409).end();
-  // }
+app.put("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((err) => next(err));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  //make list of items without the item requested to be deleted
-  const nonDeletedItems = data.entries.filter((entry) => entry.id !== id);
-  //make the data.entries array that list
-  data.entries = nonDeletedItems;
-  //is it better to do it like this? or the way above?
-  //   data.entries = data.entries.filter((entry) => entry.id !== id);
-  res.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then((result) => res.status(204).end())
+    .catch((err) => next(err));
 });
+
+app.use(unknownEndpoint);
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -14,8 +14,22 @@ mongoose
   });
 
 const phoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        // return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 phoneBookSchema.set("toJSON", {
